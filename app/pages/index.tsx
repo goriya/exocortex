74b2f2ec4bc33df06dc67cd6776da614c7fbc416
disreal48 <disreal48@gmail.com>
2021-09-03 10:12:17 +0200

    removed dead code and added some more styling

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -38,38 +38,38 @@ const UserInfo = () => {
 
 const Home: BlitzPage = () => {
   return (
-    <div className="bg-gray-50">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+    <div className="bg-blue-400">
+      <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block text-gray-900">Exocortex</span>
           <span className="block text-red-600">collect your thoughts</span>
         </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+        <div className="flex mt-8 lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white-600 bg-gray-900 hover:bg-gray-100"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium bg-gray-900 border border-transparent rounded-md text-white-600 hover:bg-gray-100"
             >
               <Link href={Routes.SignupPage()}>
                 <a>
                   {" "}
-                  <a className=" text-red-600 p-2 w-32 rounded-full hover:textColor-red-700">
+                  <a className="w-32 p-2 text-red-600 rounded-full hover:textColor-red-700">
                     <strong>sign up</strong>
                   </a>
                 </a>
               </Link>
             </a>
           </div>
-          <div className="ml-3 inline-flex rounded-md shadow">
+          <div className="inline-flex ml-3 rounded-md shadow">
             <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white-600 bg-red-600 hover:bg-gray-100"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium bg-red-600 border border-transparent rounded-md text-white-600 hover:bg-gray-100"
             >
               <>
                 <Link href={Routes.LoginPage()}>
                   <a>
                     {" "}
-                    <a className=" text-gray-900 p-2 w-32 rounded-full hover:textColor-red-700">
+                    <a className="w-32 p-2 text-gray-900 rounded-full hover:textColor-red-700">
                       <strong>login</strong>
                     </a>
                   </a>
