80d347ad1f0d2402f9f9ebec6f615359f09a978f
Moritz Reiter <moritz@anothernode.com>
2022-03-30 21:12:55 +0200

    build: adjust tailwind config to new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // tailwind.config.js
 module.exports = {
   mode: "jit",
-  purge: ["{pages,app}/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["{pages,app}/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
   theme: {
     extend: {},
   },
